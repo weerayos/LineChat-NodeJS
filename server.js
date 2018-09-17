b61a0930c625310af1af055e29cbe135085cd1ab
Weerayos Numkanisorn <weerayos@gmail.com>
2018-09-17 14:01:11 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+var cors = require('cors')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.set('port', (process.env.PORT || 4000))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+
+app.get('/', function (req, res) {
+	res.send('Hello')
+})
+
+app.listen(app.get('port'), function () {
+  console.log('run at port', app.get('port'))
+})
