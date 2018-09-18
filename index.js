6a73e609eec9beb74263c8124ae506fd7ecb8a99
Weerayos Numkanisorn <weerayos@gmail.com>
2018-09-18 19:33:15 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+const server = require('express');
+const PORT = process.env.PORT || 9999;
+const request = require('request');
+const bodyParser = require('body-parser');
+const lineMessaging = require('./src/classes/line-messaging');
+const firebase = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://chatb-bb6ea.firebaseio.com"
+});
+
+server()
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: false}))
+    .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${ PORT }`))
+    // เพิ่มส่วนของ Webhook เข้าไป
+    .post('/webhook', function (req, res) {
+        let replyToken = req.body.events[0].replyToken;
+        let message = req.body.events[0].message.text;
+        
+        console.log(`Message token : ${ replyToken }`);
+        console.log(`Message from chat : ${ message }`);
+
+        const db = firebase.database();
+        const ref = db.ref("db1/chatbot");
+        const timeRef = (new Date()).getTime();
+        
+        const chatLog = ref.child(message);
+        chatLog.set({
+          chatMsg: {
+            Chat: message,
+            Time: timeRef
+          }
+        });
+
+        lineMessaging.replyMessage(replyToken, message).then(function (rs) {
+
+            console.log(`Reply message result : ${ rs }`);
+
+            res.json({
+                status: 200,
+                message: `Sent message!`
+            });
+        });
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
