7d0d384db9997c990616b5a71a0db593d3566480
Weerayos Numkanisorn <weerayos@gmail.com>
2018-09-19 15:02:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const serviceAccount = require("./serviceAccountKey.json");
 
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
-  databaseURL: "https://chatb-bb6ea.firebaseio.com"
+  databaseURL: "xxxxxxxx"
 });
 
 server()
